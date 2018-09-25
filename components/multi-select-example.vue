5c570a3d02d702ce23c8b529d614100a2b982349
flyc <yayayahahahaooii@gmail.com>
2018-09-25 18:15:03 +0800

    binding with fake data

--- components/multi-select-example.vue
+++ components/multi-select-example.vue
@@ -3,17 +3,17 @@
       <mu-row gutter>
         <mu-col span="12" lg="4" sm="6">
           <mu-select label="Filerable Select" filterable v-model="filterable.value1" full-width>
-            <mu-option v-for="city,index in citys" :key="city" :label="city" :value="city"></mu-option>
+            <mu-option v-for="city,index in nameList" :key="city" :label="city" :value="city"></mu-option>
           </mu-select>
         </mu-col>
         <mu-col span="12" lg="4" sm="6">
           <mu-select label="Multi Filerable Select" filterable multiple v-model="filterable.value2" full-width>
-            <mu-option v-for="city,index in citys" :key="city" :label="city" :value="city"></mu-option>
+            <mu-option v-for="city,index in nameList" :key="city" :label="city" :value="city"></mu-option>
           </mu-select>
         </mu-col>
         <mu-col span="12" lg="4" sm="6">
           <mu-select label="Use Chips Filterable" filterable multiple chips v-model="filterable.value3" full-width>
-            <mu-option v-for="city,index in citys" :key="city" :label="city" :value="city"></mu-option>
+            <mu-option v-for="city,index in nameList" :key="city" :label="city" :value="city"></mu-option>
           </mu-select>
         </mu-col>
       </mu-row>
@@ -26,18 +26,34 @@
     export default {
         data() {
             return {
+                nameList: [],
                 city: '',
                 filterable: {
                     value1: '',
-                    value2: '',
-                    value3: ''
+                    value2: [],
+                    value3: []
                 }
             };
         },
-        methods: {},
+        methods: {
+            async getFake() {
+                var [data, error] = await (() => {
+                    return new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            resolve([fakeData, null])
+                        }, 300);
+                    });
+                })();
+
+                if (error) {
+                    return;
+                }
+                this.nameList = fakeData;
+            }
+        },
         mounted() {
             window.multi = this;
-            console.log(fakeData);
+            this.getFake();
         }
     };
 </script>
