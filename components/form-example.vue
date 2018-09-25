f4b603594b3a1de21884657b8c9314d65e7a8068
flyc <yayayahahahaooii@gmail.com>
2018-09-25 14:59:17 +0800

    remove some test data

--- components/form-example.vue
+++ components/form-example.vue
@@ -114,40 +114,6 @@ export default {
             // form part
             options: [
                 {
-                    key: 'key 1',
-                    label: 'option 1',
-                    value: 1
-                }, {
-                    key: 'key 2',
-                    label: 'option 2',
-                    value: 2
-                }, {
-                    key: 'key 3',
-                    label: 'option 3',
-                    value: 3
-                }, {
-                    key: 'key 4',
-                    label: 'option 4',
-                    value: 4
-                },
-                {
-                    key: 'key 5',
-                    label: 'option 5',
-                    value: 5
-                }, {
-                    key: 'key 6',
-                    label: 'option 6',
-                    value: 6
-                }, {
-                    key: 'key 7',
-                    label: 'option 7',
-                    value: 7
-                }, {
-                    key: 'key 8',
-                    label: 'option 8',
-                    value: 8
-                },
-                {
                     key: 'key 9',
                     label: 'option 9',
                     value: 9
