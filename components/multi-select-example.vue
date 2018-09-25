a5228871da4649ce65df5e9d9b84f3bd5d8cb8a2
flyc <yayayahahahaooii@gmail.com>
2018-09-25 18:16:03 +0800

    modified text of city to singleName

--- components/multi-select-example.vue
+++ components/multi-select-example.vue
@@ -3,17 +3,17 @@
       <mu-row gutter>
         <mu-col span="12" lg="4" sm="6">
           <mu-select label="Filerable Select" filterable v-model="filterable.value1" full-width>
-            <mu-option v-for="city,index in nameList" :key="city" :label="city" :value="city"></mu-option>
+            <mu-option v-for="singleName, index in nameList" :key="singleName" :label="singleName" :value="singleName"></mu-option>
           </mu-select>
         </mu-col>
         <mu-col span="12" lg="4" sm="6">
           <mu-select label="Multi Filerable Select" filterable multiple v-model="filterable.value2" full-width>
-            <mu-option v-for="city,index in nameList" :key="city" :label="city" :value="city"></mu-option>
+            <mu-option v-for="singleName, index in nameList" :key="singleName" :label="singleName" :value="singleName"></mu-option>
           </mu-select>
         </mu-col>
         <mu-col span="12" lg="4" sm="6">
           <mu-select label="Use Chips Filterable" filterable multiple chips v-model="filterable.value3" full-width>
-            <mu-option v-for="city,index in nameList" :key="city" :label="city" :value="city"></mu-option>
+            <mu-option v-for="singleName, index in nameList" :key="singleName" :label="singleName" :value="singleName"></mu-option>
           </mu-select>
         </mu-col>
       </mu-row>
