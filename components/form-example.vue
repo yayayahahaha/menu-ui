7546a410256c993d37d6be7405ffff3d8e1574b5
flyc <yayayahahahaooii@gmail.com>
2018-09-25 12:08:47 +0800

    paste whole form and add scrollable stuff

--- components/form-example.vue
+++ components/form-example.vue
@@ -1,8 +1,58 @@
 <template>
     <mu-container>
         <mu-button class="red-color" color="#0C0A3C" @click="openAlertDialog">Open Dialog</mu-button>
-        <mu-dialog title="Use Google's location service?" width="600" max-width="80%" :esc-press-close="false" :overlay-close="false" :open.sync="openAlert">
-        Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
+        <mu-dialog
+            title="Use Google's location service?"
+            width="600"
+            max-width="80%"
+            :esc-press-close="false"
+            :overlay-close="false"
+            :open.sync="openAlert"
+            scrollable>
+            <mu-row>
+                <mu-col>
+                    <mu-container>
+                      <mu-flex align-items="center" style="padding-bottom: 8px;">
+                        <span style="margin-right: 16px;">Label Position:</span>
+                        <mu-radio v-model="labelPosition" style="margin-right: 16px;" value="top" label="Top"></mu-radio>
+                        <mu-radio v-model="labelPosition" style="margin-right: 16px;" value="left" label="Left"></mu-radio>
+                        <mu-radio v-model="labelPosition" style="margin-right: 16px;" value="right" label="Right"></mu-radio>
+                      </mu-flex>
+                      <mu-form :model="form" class="mu-demo-form" :label-position="labelPosition" label-width="100">
+                        <mu-form-item prop="input" label="Input">
+                          <mu-text-field v-model="form.input"></mu-text-field>
+                        </mu-form-item>
+                        <mu-form-item prop="select" label="Select">
+                          <mu-select v-model="form.select">
+                            <mu-option v-for="option,index in options" :key="option" :label="option" :value="option"></mu-option>
+                          </mu-select>
+                        </mu-form-item>
+                        <mu-form-item prop="date" label="Date Time">
+                          <mu-date-input v-model="form.date" type="dateTime" actions></mu-date-input>
+                        </mu-form-item>
+                        <mu-form-item prop="radio" label="Radio">
+                          <mu-radio v-model="form.radio" value="male" label="Male"></mu-radio>
+                          <mu-radio v-model="form.radio" value="female" label="Female"></mu-radio>
+                        </mu-form-item>
+                        <mu-form-item prop="checkbox" label="Checkbox">
+                          <mu-checkbox v-model="form.checkbox" value="eat" label="Eat"></mu-checkbox>
+                          <mu-checkbox v-model="form.checkbox" value="sleep" label="Sleep"></mu-checkbox>
+                          <mu-checkbox v-model="form.checkbox" value="run" label="Run"></mu-checkbox>
+                          <mu-checkbox v-model="form.checkbox" value="movie" label="Movie"></mu-checkbox>
+                        </mu-form-item>
+                        <mu-form-item prop="switch" label="Switch">
+                          <mu-switch v-model="form.switch"></mu-switch>
+                        </mu-form-item>
+                        <mu-form-item prop="slider" label="Slider">
+                          <mu-slider v-model="form.slider"></mu-slider>
+                        </mu-form-item>
+                        <mu-form-item prop="textarea" label="Textarea">
+                          <mu-text-field multi-line :rows="3" :rows-max="6" v-model="form.textarea"></mu-text-field>
+                        </mu-form-item>
+                      </mu-form>
+                    </mu-container>
+                </mu-col>
+            </mu-row>
             <mu-button slot="actions" flat color="primary" @click="closeAlertDialog">Disagree</mu-button>
             <mu-button slot="actions" flat color="primary" @click="closeAlertDialog">Agree</mu-button>
         </mu-dialog>
